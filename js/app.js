7e3dd20e30952b3b43344cb22f73bad43a7d3206
unknown <pateldhwani59@gmail.com>
2020-11-12 19:11:56 -0800

    w9a-variables and conditionals

---
+++ js/app.js
@@ -0,0 +1,20 @@
+var age = prompt("how old you are?");
+var is_subscribed = confirm("Do you have subscribtion");
+
+
+if (age < 18 && is_subscribed == false){
+    console.log("The user is younger than 18 and not subscribed");
+}
+else if(age >= 18 && is_subscribed == false){
+    console.log("The user is 18 and older and not subscribed");
+}
+else if(age < 18 && is_subscribed == true){
+    console.log("The user is younger than 18 and is subscribed");
+}
+else if(age >= 18 && is_subscribed == true){
+    console.log("The user is 18 and older and is subscribed")
+}
+else{
+    console.log("enjoy the party")
+}
+
