19dc266998b9ccce6520a1332082a267b56aa9b4
unknown <pateldhwani59@gmail.com>
2020-11-14 00:43:00 -0800

    w9b-simple user array

--- js/app.js
+++ js/app.js
@@ -1,18 +1,48 @@
-var age = prompt("how old you are?");
-var is_subscribed = confirm("Do you have subscribtion");
 
 
-if (age < 18 && is_subscribed == false){
-    console.log("The user is younger than 18 and not subscribed");
-}
-else if(age >= 18 && is_subscribed == false){
-    console.log("The user is 18 or older and not subscribed");
-}
-else if(age < 18 && is_subscribed == true){
-    console.log("The user is younger than 18 and is subscribed");
-}
-else {
-    console.log("The user is 18 or older and is subscribed")
-}
+var age = [
+    23,
+    12,
+    56,
+    13,
+    
+    
+    
+];
+var is_subscribed = [
+    'true',
+    'false',
+    'true',
+    'false',
+    
+];
+
+
+
+
+
+
+
+
+ for (var i = 0; i < age.length; i++){
+    if (age[i] < 18 && is_subscribed[i] == 'false'){
+        console.log("The user is younger than 18 and not subscribed");
+    }
+    else if(age[i] >= 18 && is_subscribed[i] == 'false'){
+        console.log("The user is 18 or older and not subscribed");
+    }
+    else if(age[i] < 18 && is_subscribed[i] == 'true'){
+        console.log("The user is younger than 18 and is subscribed");
+    }
+    else {
+        console.log("The user is 18 or older and is subscribed")
+    }
+    
+     
+ }
+
+//  for (var i = 0; i < is_subscribed.length; i++){
+//      console.log(is_subscribed[i]);
+//  }
 
 
